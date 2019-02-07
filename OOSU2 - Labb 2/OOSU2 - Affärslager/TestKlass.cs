fbb863466ef3f37f69f87b37dbd34ac533bb9c2e
ImNallen <47414671+ImNallen@users.noreply.github.com>
2019-02-07 11:03:05 +0100

    Test för att se hur updates fungerar

---
+++ OOSU2 - Labb 2/OOSU2 - Affärslager/TestKlass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOSU2___Affärslager
+{
+    class TestKlass
+    {
+        private string namn;
+    }
+}
